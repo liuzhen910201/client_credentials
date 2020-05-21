62bfb083fb44ac1864746dbbd0bc227140315859
aha05256 <aha05256@jp.ibm.com>
2020-05-21 11:20:54 +0900

    删除授权码相关内容

--- db/index.js
+++ db/index.js
@@ -3,11 +3,11 @@
 const users = require('./users');
 const clients = require('./clients');
 const accessTokens = require('./access_tokens');
-const authorizationCodes = require('./authorization_codes');
+// const authorizationCodes = require('./authorization_codes');
 
 module.exports = {
   users,
   clients,
   accessTokens,
-  authorizationCodes,
+  // authorizationCodes,
 };
